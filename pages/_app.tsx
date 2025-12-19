7295aab333d2852f43f2a5ddb1e3e8b0687a7ea3
Your Name <you@example.com>
2025-12-19 11:19:36 +0400

    Fix mobile viewport and polish dashboard layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import { AppProps } from 'next/app'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { SessionProvider } from "next-auth/react"
+// pages/_app.tsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { SessionProvider } from 'next-auth/react';
 import { MantineProvider } from '@mantine/core';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-    <SessionProvider session={pageProps.session}>
-      <>
-        <Component {...pageProps} />
-        <ToastContainer position="top-right" autoClose={3000} />
-      </>
-    </SessionProvider>
-    </MantineProvider>
-  )
+    <>
+      {/* ✅ This fixes the mobile/tablet layout issue */}
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+      </Head>
+
+      <MantineProvider>
+        <SessionProvider session={pageProps.session}>
+          <>
+            <Component {...pageProps} />
+            <ToastContainer position="top-right" autoClose={3000} />
+          </>
+        </SessionProvider>
+      </MantineProvider>
+    </>
+  );
 }
