b9fffdb6973be85b0423e37e32bd698474b052df
Your Name <you@example.com>
2025-12-25 10:46:47 +0400

    Fix mobile overflow + clean store slider + improve mobile layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 // pages/_app.tsx
+import "../styles/globals.css"; 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { AppProps } from 'next/app';
