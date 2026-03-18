8039988f9ff98dbe245633e097af2469a2570456
Your Name <you@example.com>
2026-03-18 10:14:46 +0400

    Add Vercel Analytics fix next config and Fix favicon with SVG

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,29 @@
-// pages/_app.tsx
-import "../styles/globals.css"; 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import { AppProps } from 'next/app';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { SessionProvider } from 'next-auth/react';
-import { MantineProvider } from '@mantine/core';
-import Head from 'next/head';
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
 
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+import { MantineProvider } from "@mantine/core";
+import { ToastContainer } from "react-toastify";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* ✅ This fixes the mobile/tablet layout issue */}
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        />
+        <title>Cross Border Cart</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Browser tab icon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
 
       <MantineProvider>
         <SessionProvider session={pageProps.session}>
-          <>
-            <Component {...pageProps} />
-            <ToastContainer position="top-right" autoClose={3000} />
-          </>
+          <Component {...pageProps} />
+          <ToastContainer position="top-right" autoClose={3000} />
         </SessionProvider>
       </MantineProvider>
     </>
