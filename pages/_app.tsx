428a86814fbf5fe90807d9379bf1437475d41573
Your Name <you@example.com>
2026-08-07 11:49:09 +0400

    Add Google Analytics (GA4) tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,12 @@ import "react-toastify/dist/ReactToastify.css";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 import { SessionProvider } from "next-auth/react";
 import { MantineProvider } from "@mantine/core";
 import { ToastContainer } from "react-toastify";
+
+const GA_MEASUREMENT_ID = "G-3H0471HBRN";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -19,6 +22,20 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         {/* Browser tab icon */}
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </Head>
+
+      {/* Google Analytics (GA4) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
 
       <MantineProvider>
         <SessionProvider session={pageProps.session}>
